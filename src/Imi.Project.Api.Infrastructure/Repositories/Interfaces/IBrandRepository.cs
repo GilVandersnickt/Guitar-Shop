dc2b4bba949018b91ef7adcc3be088be4aab8791
gilvandersnickt <gil.vandersnickt@student.howest.be>
2021-10-22 01:50:05 +0200

    Update interface brand repository with tasks

--- src/Imi.Project.Api.Infrastructure/Repositories/Interfaces/IBrandRepository.cs
+++ src/Imi.Project.Api.Infrastructure/Repositories/Interfaces/IBrandRepository.cs
@@ -2,10 +2,15 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Imi.Project.Api.Infrastructure.Repositories.Interfaces
 {
     public interface IBrandRepository : IBaseRepository<Brand>
     {
+        Task<IEnumerable<Brand>> GetByCategoryIdAsync(Guid id);
+        Task<IEnumerable<Brand>> GetBySubcategoryIdAsync(Guid id);
+        Task<IEnumerable<Brand>> SearchAsync(string search);
+
     }
 }
