5c0e94f69255fe9d6cb1a537136aaeb5b3fa1aa7
gilvandersnickt <gil.vandersnickt@student.howest.be>
2021-10-22 01:48:49 +0200

    Add interface brand repository

---
+++ src/Imi.Project.Api.Infrastructure/Repositories/Interfaces/IBrandRepository.cs
@@ -0,0 +1,11 @@
+﻿using Imi.Project.Api.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imi.Project.Api.Infrastructure.Repositories.Interfaces
+{
+    public interface IBrandRepository : IBaseRepository<Brand>
+    {
+    }
+}
