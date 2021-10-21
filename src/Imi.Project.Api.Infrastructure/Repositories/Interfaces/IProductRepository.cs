b44828662ed9857c98ce5211756651dd92a09f96
gilvandersnickt <gil.vandersnickt@student.howest.be>
2021-10-22 01:39:33 +0200

    Add interface product repository

---
+++ src/Imi.Project.Api.Infrastructure/Repositories/Interfaces/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using Imi.Project.Api.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Api.Infrastructure.Repositories.Interfaces
+{
+    public interface IProductRepository : IBaseRepository<Product>
+    {
+        Task<IEnumerable<Product>> GetByBrandIdAsync(Guid id);
+        Task<IEnumerable<Product>> GetByCategoryIdAsync(Guid id);
+        Task<IEnumerable<Product>> GetBySubcategoryIdAsync(Guid id);
+        Task<IEnumerable<Product>> SearchAsync(string search);
+    }
+}
