45e6301e59adf65620d452be3427ee0febe3fc65
gilvandersnickt <gil.vandersnickt@student.howest.be>
2021-10-22 01:58:12 +0200

    Add product repository

---
+++ src/Imi.Project.Api.Infrastructure/Repositories/BrandRepository.cs
@@ -0,0 +1,17 @@
+﻿using Imi.Project.Api.Entities;
+using Imi.Project.Api.Infrastructure.Data;
+using Imi.Project.Api.Infrastructure.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imi.Project.Api.Infrastructure.Repositories
+{
+    public class BrandRepository : BaseRepository<Brand>, IBrandRepository
+    {
+        public BrandRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+
+        }
+    }
+}
