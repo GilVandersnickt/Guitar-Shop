2868b6e8ca9029b082b9a6e7f9ad9be8c185d8e8
gilvandersnickt <gil.vandersnickt@student.howest.be>
2021-10-22 17:21:11 +0200

    Add brand response dto

---
+++ src/Imi.Project.Api.Core/Dtos/BrandResponseDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Imi.Project.Api.Core.Dtos
+{
+    public class BrandResponseDto : DtoBase
+    {
+        public string Name { get; set; }
+        public Uri Image { get; set; }
+        public ICollection<ProductResponseDto> Products { get; set; }
+        public ICollection<CategoryResponseDto> Categories { get; set; }
+        public ICollection<SubcategoryResponseDto> Subcategories { get; set; }
+    }
+}
