0635eff60e828a1d1f261dd0fb03e720198320b1
gilvandersnickt <gil.vandersnickt@student.howest.be>
2021-10-22 04:01:03 +0200

    Add brands controller

---
+++ src/Imi.Project.Api/Controllers/BrandsController.cs
@@ -0,0 +1,21 @@
+﻿using Imi.Project.Api.Infrastructure.Repositories.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandsController : ControllerBase
+    {
+        protected readonly IBrandRepository _brandRepository;
+        public BrandsController(IBrandRepository brandRepository)
+        {
+            _brandRepository = brandRepository;
+        }
+    }
+}
