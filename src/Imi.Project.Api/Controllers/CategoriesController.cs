622885b792e711e1851a1ebb910e0275e6e00d0b
gilvandersnickt <gil.vandersnickt@student.howest.be>
2021-10-22 04:01:16 +0200

    Add categories controller

---
+++ src/Imi.Project.Api/Controllers/CategoriesController.cs
@@ -0,0 +1,21 @@
+﻿using Imi.Project.Api.Infrastructure.Repositories.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        protected readonly ICategoryRepository _categoryRepository; 
+        public CategoriesController(ICategoryRepository categoryRepository) 
+        { 
+            _categoryRepository = categoryRepository; 
+        }
+    }
+}
