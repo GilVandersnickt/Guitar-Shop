f03a3382e27c5dc6ed6f9f775878614acd0b1948
gilvandersnickt <gil.vandersnickt@student.howest.be>
2021-10-22 21:19:58 +0200

    Add subcategory service

---
+++ src/Imi.Project.Api.Core/Services/SubcategoryService.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using Imi.Project.Api.Core.Dtos;
+using Imi.Project.Api.Core.Interfaces.Repositories;
+using Imi.Project.Api.Core.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Api.Core.Services
+{
+    public class SubcategoryService : ISubcategoryService
+    {
+        private readonly ISubcategoryRepository _subcategoryRepository;
+        private readonly IMapper _mapper;
+
+        public SubcategoryService(ISubcategoryRepository subcategoryRepository, IMapper mapper)
+        {
+            _subcategoryRepository = subcategoryRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<SubcategoryResponseDto> GetByIdAsync(Guid id)
+        {
+            var result = await _subcategoryRepository.GetByIdAsync(id);
+            var dto = _mapper.Map<SubcategoryResponseDto>(result);
+            return dto;
+        }
+
+        public async Task<IEnumerable<SubcategoryResponseDto>> ListAllAsync()
+        {
+            var result = await _subcategoryRepository.ListAllAsync();
+            var dto = _mapper.Map<IEnumerable<SubcategoryResponseDto>>(result);
+            return dto;
+        }
+        public async Task<IEnumerable<SubcategoryResponseDto>> GetByBrandIdAsync(Guid id)
+        {
+            var result = await _subcategoryRepository.GetByBrandIdAsync(id);
+            var dto = _mapper.Map<IEnumerable<SubcategoryResponseDto>>(result);
+            return dto;
+        }
+
+        public async Task<IEnumerable<SubcategoryResponseDto>> GetByCategoryIdAsync(Guid id)
+        {
+            var result = await _subcategoryRepository.GetByCategoryIdAsync(id);
+            var dto = _mapper.Map<IEnumerable<SubcategoryResponseDto>>(result);
+            return dto;
+        }
+    }
+}
