a8ecdce8845c36e325d1bed4bd91959a0ed1ee0f
gilvandersnickt <gil.vandersnickt@student.howest.be>
2021-10-22 04:56:08 +0200

    Update product seeding

--- src/Imi.Project.Api.Infrastructure/Data/Seeding/ProductSeeder.cs
+++ src/Imi.Project.Api.Infrastructure/Data/Seeding/ProductSeeder.cs
@@ -16,18 +16,18 @@ namespace Imi.Project.Api.Infrastructure.Data.Seeding
                     Id = Guid.Parse("00000000-0000-0000-0000-000000000001"),
                     Name = "Fender Stratocaster",
                     Price = 1000M,
-                    BrandId = Guid.Parse("00000000-0000-0000-0000-000000000001"),
-                    CategoryId = Guid.Parse("00000000-0000-0000-0000-000000000001"),
-                    SubcategoryId = Guid.Parse("00000000-0000-0000-0000-000000000001"),
+                    BrandId = Guid.Parse("00000000-0000-0000-0001-000000000001"),
+                    CategoryId = Guid.Parse("00000000-0000-0000-0002-000000000001"),
+                    SubcategoryId = Guid.Parse("00000000-0000-0000-0003-000000000001"),
                 },
                 new Product
                 {
                     Id = Guid.Parse("00000000-0000-0000-0000-000000000002"),
                     Name = "Marshall JVM",
                     Price = 1000M,
-                    BrandId = Guid.Parse("00000000-0000-0000-0000-000000000002"),
-                    CategoryId = Guid.Parse("00000000-0000-0000-0000-000000000002"),
-                    SubcategoryId = Guid.Parse("00000000-0000-0000-0000-000000000002"),
+                    BrandId = Guid.Parse("00000000-0000-0000-0001-000000000002"),
+                    CategoryId = Guid.Parse("00000000-0000-0000-0002-000000000002"),
+                    SubcategoryId = Guid.Parse("00000000-0000-0000-0003-000000000002"),
                 }
             );
         }
