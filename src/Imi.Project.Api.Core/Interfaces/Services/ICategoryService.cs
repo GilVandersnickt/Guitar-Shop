18bf7ce111b231f159e6efe4888476792e4d7860
gilvandersnickt <gil.vandersnickt@student.howest.be>
2021-10-22 21:18:45 +0200

    Add interface for category service

---
+++ src/Imi.Project.Api.Core/Interfaces/Services/ICategoryService.cs
@@ -0,0 +1,14 @@
+﻿using Imi.Project.Api.Core.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Api.Core.Interfaces.Services
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<CategoryResponseDto>> ListAllAsync();
+        Task<CategoryResponseDto> GetByIdAsync(Guid id);
+        Task<IEnumerable<CategoryResponseDto>> GetByBrandIdAsync(Guid id);
+    }
+}
