191e03d4cc8d6d48fc8d4eea963084ee5e0eef38
gilvandersnickt <gil.vandersnickt@student.howest.be>
2021-10-22 17:21:19 +0200

    Add category response dto

---
+++ src/Imi.Project.Api.Core/Dtos/CategoryResponseDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Imi.Project.Api.Core.Dtos
+{
+    public class CategoryResponseDto : DtoBase
+    {
+        public string Name { get; set; }
+        public Uri Image { get; set; }
+        public ICollection<ProductResponseDto> Products { get; set; }
+        public ICollection<BrandResponseDto> Brands { get; set; }
+        public ICollection<SubcategoryResponseDto> Subcategories { get; set; }
+    }
+}
