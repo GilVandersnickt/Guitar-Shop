f10c30660bba7e6cd5fed98c5c2c9f8ed141a9c9
gilvandersnickt <gil.vandersnickt@student.howest.be>
2021-10-22 21:26:17 +0200

    Add interface for subcategory repository

---
+++ src/Imi.Project.Api.Core/Interfaces/Repositories/ISubcategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using Imi.Project.Api.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Api.Core.Interfaces.Repositories
+{
+    public interface ISubcategoryRepository : IRepository<Subcategory>
+    {
+        Task<IEnumerable<Subcategory>> GetByBrandIdAsync(Guid id);
+        Task<IEnumerable<Subcategory>> GetByCategoryIdAsync(Guid id);
+    }
+}
