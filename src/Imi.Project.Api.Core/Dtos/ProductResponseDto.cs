a3aed1b158d18ce5142459d7cfbcff4b5b5b5957
gilvandersnickt <gil.vandersnickt@student.howest.be>
2021-10-22 17:21:01 +0200

    Add product response dto

---
+++ src/Imi.Project.Api.Core/Dtos/ProductResponseDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Imi.Project.Api.Core.Dtos
+{
+    public class ProductResponseDto : DtoBase
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public Uri Image { get; set; }
+        public BrandResponseDto Brand { get; set; }
+        public CategoryResponseDto Category { get; set; }
+        public SubcategoryResponseDto Subcategory { get; set; }
+    }
+}
