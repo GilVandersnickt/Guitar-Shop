fb4c2355c999572ca42cd54334d72cee2efa401d
gilvandersnickt <gil.vandersnickt@student.howest.be>
2021-10-23 00:52:26 +0200

    Update repositories to fix errors

--- src/Imi.Project.Api.Infrastructure/Repositories/ProductRepository.cs
+++ src/Imi.Project.Api.Infrastructure/Repositories/ProductRepository.cs
@@ -1,6 +1,6 @@
-﻿using Imi.Project.Api.Entities;
+﻿using Imi.Project.Api.Core.Interfaces.Repositories;
+using Imi.Project.Api.Entities;
 using Imi.Project.Api.Infrastructure.Data;
-using Imi.Project.Api.Infrastructure.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace Imi.Project.Api.Infrastructure.Repositories
 {
-    public class ProductRepository : EfRepository<Product>, IProductRepository
+    public class ProductRepository : BaseRepository<Product>, IProductRepository
     {
         public ProductRepository(ApplicationDbContext dbContext) : base(dbContext)
         {
@@ -22,16 +22,19 @@ namespace Imi.Project.Api.Infrastructure.Repositories
                 .Include(p => p.Category)
                 .Include(p => p.Subcategory);
         }
+
         public async override Task<IEnumerable<Product>> ListAllAsync()
         {
             var products = await GetAllAsync().ToListAsync();
             return products;
         }
+
         public async override Task<Product> GetByIdAsync(Guid id)
         {
             var product = await GetAllAsync().SingleOrDefaultAsync(p => p.Id.Equals(id));
             return product;
         }
+
         public async Task<IEnumerable<Product>> GetByBrandIdAsync(Guid id)
         {
             var products = await GetAllAsync().Where(p => p.BrandId.Equals(id)).ToListAsync();
@@ -47,6 +50,7 @@ namespace Imi.Project.Api.Infrastructure.Repositories
             var products = await GetAllAsync().Where(p => p.CategoryId.Equals(id)).ToListAsync();
             return products;
         }
+
         public async Task<IEnumerable<Product>> SearchAsync(string search)
         {
             var products = await GetAllAsync()
