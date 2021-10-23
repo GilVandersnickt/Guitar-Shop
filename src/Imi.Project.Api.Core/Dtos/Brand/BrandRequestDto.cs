68f507da0ea9f7fc1a4f8d6f255b53852afda88e
gilvandersnickt <gil.vandersnickt@student.howest.be>
2021-10-23 20:09:20 +0200

    Clean dtos folder

--- src/Imi.Project.Api.Core/Dtos/Brand/BrandRequestDto.cs
+++ src/Imi.Project.Api.Core/Dtos/Brand/BrandRequestDto.cs
@@ -1,13 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Text;
+
 namespace Imi.Project.Api.Core.Dtos
 {
-    public class SubcategoryRequestDto
+    public class BrandRequestDto
     {
         [Required(ErrorMessage = "{0} is required")]
         public string Name { get; set; }
-        public Guid CategoryId { get; set; }
+        public Uri Image { get; set; }
     }
 }
