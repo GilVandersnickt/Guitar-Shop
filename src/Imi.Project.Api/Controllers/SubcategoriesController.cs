d4a12200cf4162469759f7814dfdf47e55f1dc50
gilvandersnickt <gil.vandersnickt@student.howest.be>
2021-10-23 04:02:53 +0200

    Add subcategories controller

---
+++ src/Imi.Project.Api/Controllers/SubcategoriesController.cs
@@ -0,0 +1,76 @@
+﻿using Imi.Project.Api.Core.Dtos;
+using Imi.Project.Api.Core.Interfaces.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubcategoriesController : ControllerBase
+    {
+        private readonly ISubcategoryService _subcategoryService;
+
+        public SubcategoriesController(ISubcategoryService subcategoryService)
+        {
+            _subcategoryService = subcategoryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var subcategories = await _subcategoryService.ListAllAsync();
+            return Ok(subcategories);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            var subcategory = await _subcategoryService.GetByIdAsync(id);
+            if (subcategory == null)
+            {
+                return NotFound($"Subcategory with ID {id} does not exist");
+            }
+            return Ok(subcategory);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(SubcategoryRequestDto subcategoryRequestDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var subcategoryResponseDto = await _subcategoryService.AddAsync(subcategoryRequestDto);
+
+            return CreatedAtAction(nameof(Get), new { id = subcategoryResponseDto.Id }, subcategoryResponseDto);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put(SubcategoryRequestDto subcategoryRequestDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var subcategoryResponseDto = await _subcategoryService.UpdateAsync(subcategoryRequestDto);
+            return Ok(subcategoryResponseDto);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var subcategory = await _subcategoryService.GetByIdAsync(id);
+            if (subcategory == null)
+            {
+                return NotFound($"Subcategory with ID {id} does not exist");
+            }
+            await _subcategoryService.DeleteAsync(id);
+            return Ok();
+        }
+    }
+}
