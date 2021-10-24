90985502dde68baf63d7a7ab9a8969b1674e79cf
gilvandersnickt <gil.vandersnickt@student.howest.be>
2021-10-24 04:53:26 +0200

    Update request dtos to inherit dto base

--- src/Imi.Project.Api.Core/Dtos/Category/CategoryRequestDto.cs
+++ src/Imi.Project.Api.Core/Dtos/Category/CategoryRequestDto.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Imi.Project.Api.Core.Dtos
 {
-    public class CategoryRequestDto
+    public class CategoryRequestDto : DtoBase
     {
         [Required(ErrorMessage = "{0} is required")]
         public string Name { get; set; }
