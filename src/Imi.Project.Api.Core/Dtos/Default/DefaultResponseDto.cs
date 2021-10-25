aa2134c8ac9f5bb055f16594487619276dd7e4f6
gilvandersnickt <gil.vandersnickt@student.howest.be>
2021-10-25 14:26:27 +0200

    Add default response dto

---
+++ src/Imi.Project.Api.Core/Dtos/Default/DefaultResponseDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imi.Project.Api.Core.Dtos.Partials
+{
+    public class DefaultResponseDto : DtoBase
+    {
+        public string Name { get; set; }
+    }
+}
