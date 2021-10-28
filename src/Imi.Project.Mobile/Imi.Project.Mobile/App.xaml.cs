f646e14a0e52ffd9c9e769a749377c4ff2e9ca92
gilvandersnickt <gil.vandersnickt@student.howest.be>
2021-10-28 13:31:56 +0200

    Rename main page in App.xaml.cs

--- src/Imi.Project.Mobile/Imi.Project.Mobile/App.xaml.cs
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/App.xaml.cs
@@ -10,7 +10,7 @@ namespace Imi.Project.Mobile
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage( new MainPage());
+            MainPage = new NavigationPage( new HomePage());
         }
 
         protected override void OnStart()
