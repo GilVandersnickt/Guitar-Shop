bc9c344876cb286adace271482c29a43b90683c8
gilvandersnickt <gil.vandersnickt@student.howest.be>
2021-11-01 05:04:54 +0100

    Add interface for memory game service

---
+++ src/Imi.Project.Blazor/Services/Interfaces/IMemoryGameService.cs
@@ -0,0 +1,15 @@
+﻿using Imi.Project.Blazor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Blazor.Services.Interfaces
+{
+    public interface IMemoryGameService
+    {
+        void StartGame();
+        void ResetGame();
+        Task Flip(MemoryGameCard card);
+    }
+}
