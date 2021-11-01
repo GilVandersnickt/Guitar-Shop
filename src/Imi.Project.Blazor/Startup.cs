bbeb370244ce4446d34e9d938c33d3b6beb98ff4
gilvandersnickt <gil.vandersnickt@student.howest.be>
2021-11-01 05:54:42 +0100

    Clean startup class

--- src/Imi.Project.Blazor/Startup.cs
+++ src/Imi.Project.Blazor/Startup.cs
@@ -1,17 +1,10 @@
-using Imi.Project.Blazor.Data;
 using Imi.Project.Blazor.Models;
 using Imi.Project.Blazor.Services;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Imi.Project.Blazor
 {
