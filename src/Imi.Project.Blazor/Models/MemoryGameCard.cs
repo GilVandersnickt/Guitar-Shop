67cb6a9353ab3119520856691ab90c333bcf4f54
gilvandersnickt <gil.vandersnickt@student.howest.be>
2021-11-01 05:05:14 +0100

    Add memory game card model

---
+++ src/Imi.Project.Blazor/Models/MemoryGameCard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Blazor.Models
+{
+    public class MemoryGameCard
+    {
+        public int number { get; set; }
+        public string color { get; set; }
+        public string image { get { return flipped ? "Guitar" + number.ToString() + ".PNG" : string.Empty; } }
+        public bool flipped { get; set; }
+
+    }
+}
