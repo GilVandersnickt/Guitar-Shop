d016c1cee2b7fe7892c67342cce48a7cf8bf87e3
gilvandersnickt <gil.vandersnickt@student.howest.be>
2021-11-01 04:49:03 +0100

    Add product item model

---
+++ src/Imi.Project.Blazor/Models/ProductItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Blazor.Models
+{
+    public class ProductItem
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(500, ErrorMessage = "Title is too long.")]
+        public string Title { get; set; }
+        public string CategoryId { get; set; }
+        public InputSelectItem[] Categories { get; set; }
+        public string Price { get; set; }
+    }
+}
