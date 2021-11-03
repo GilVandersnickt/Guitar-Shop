b6af4a3dfdbc890aa860ede56ec27c9657c967c3
gilvandersnickt <gil.vandersnickt@student.howest.be>
2021-11-03 04:31:35 +0100

    Add crud product page for deleting

---
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/Pages/CRUDProductDeletePage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Imi.Project.Mobile.Domain.Models;
+using Imi.Project.Mobile.Domain.Services;
+using Imi.Project.Mobile.Domain.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Imi.Project.Mobile.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CRUDProductDeletePage : ContentPage
+    {
+        private readonly IProductService productService = new ProductService();
+
+        public List<Product> Products { get => GetProducts(); }
+
+
+        public CRUDProductDeletePage()
+        {
+            InitializeComponent();
+            this.BindingContext = this;
+        }
+        private List<Product> GetProducts()
+        {
+            return productService.Get().Result;
+        }
+
+    }
+}
