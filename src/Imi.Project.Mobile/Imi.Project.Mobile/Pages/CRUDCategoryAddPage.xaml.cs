20494c19b5fd9eb95571a9ff3baa2d1971fac94a
gilvandersnickt <gil.vandersnickt@student.howest.be>
2021-11-03 04:30:08 +0100

    Add crud category page for adding

---
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/Pages/CRUDCategoryAddPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Imi.Project.Mobile.Domain.Models;
+using Imi.Project.Mobile.Domain.Services;
+using Imi.Project.Mobile.Domain.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Imi.Project.Mobile.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CRUDCategoryAddPage : ContentPage
+    {
+        private readonly ICategoryService categoryService = new CategoryService();
+
+        public List<Category> Categories { get => GetCategories(); }
+
+        public CRUDCategoryAddPage()
+        {
+            InitializeComponent();
+            this.BindingContext = this;
+        }
+        private List<Category> GetCategories()
+        {
+            return categoryService.Get().Result;
+        }
+
+    }
+}
