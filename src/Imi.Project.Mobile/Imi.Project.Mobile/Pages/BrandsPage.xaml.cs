0a9a8526068b0111261930550b13ae50cf01235d
gilvandersnickt <gil.vandersnickt@student.howest.be>
2021-11-03 23:01:46 +0100

    Update brands page

--- src/Imi.Project.Mobile/Imi.Project.Mobile/Pages/BrandsPage.xaml.cs
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/Pages/BrandsPage.xaml.cs
@@ -17,7 +17,6 @@ namespace Imi.Project.Mobile.Pages
     {
         private readonly IBrandService brandService = new BrandService();
         public List<Brand> Brands { get => GetBrands(); }
-        //public static BindableProperty ItemClickCommandProperty = BindableProperty.Create(nameof(ItemClickCommand), typeof(ICommand), typeof(Brand), null);
 
         public BrandsPage()
         {
@@ -29,57 +28,6 @@ namespace Imi.Project.Mobile.Pages
             return brandService.Get().Result;
         }
 
-        //public ICommand OpenItemPageCommand => new Command<Brand>(
-        //    async (Brand brand) => {              
-        //        var productsPage = new ProductsPage(brand);
-        //       await Navigation.PushAsync(productsPage, true);
-        //    }
-        //);
-
-        //public ICommand ItemTapped
-        //{
-        //    get
-        //    {
-        //        return new Command((brand) =>
-        //        {
-        //            var productsPage = new ProductsPage(brand as Brand);
-        //            Navigation.PushAsync(productsPage, true);
-        //        });
-        //    }
-        //}
-        //private void OnItemTapped(object sender, ItemTappedEventArgs e)
-        //{
-        //    if (e.Item != null)
-        //    {
-        //        var brand = e.Item as Brand;
-        //        Navigation.PushAsync(new ProductsPage(brand));
-        //    }
-        //}
-
-
-        //private void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
-        //{
-        //    var test = e.Parameter as Brand;
-        //    Navigation.PushAsync(new ProductsPage(test));
-        //    //if (e.Item != null)
-        //    //{
-        //    //var brand = e.Item as Brand;
-        //    //Navigation.PushAsync(new ProductsPage(brand));
-        //    //}
-
-        //}
-
-        //private void ImageButton_Clicked(object sender, EventArgs e)
-        //{
-
-        //    if (e != null)
-        //    {
-        //        //var brand = e.Item as Brand;
-        //        Navigation.PushAsync(new ProductsPage());
-        //    }
-
-        //}
-
         private async void cvBrands_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             var brand = e.CurrentSelection.FirstOrDefault() as Brand;
@@ -88,18 +36,5 @@ namespace Imi.Project.Mobile.Pages
             await Navigation.PushAsync(new ProductsPage(brand));
             ((CollectionView)sender).SelectedItem = null;
         }
-
-        //public ICommand ItemClickCommand
-        //{
-        //    get
-        //    {
-        //        return (ICommand)this.GetValue(ItemClickCommandProperty);
-        //    }
-        //    set
-        //    {
-        //        this.SetValue(ItemClickCommandProperty, value);
-        //    }
-        //}
-
     }
 }
