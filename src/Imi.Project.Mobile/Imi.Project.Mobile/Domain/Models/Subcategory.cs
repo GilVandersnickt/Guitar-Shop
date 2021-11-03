d0afc1f9842491ce272e4d93958d9bbd91f462b8
gilvandersnickt <gil.vandersnickt@student.howest.be>
2021-11-03 03:49:30 +0100

    Add subcategory model

---
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/Domain/Models/Subcategory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imi.Project.Mobile.Domain.Models
+{
+    public class Subcategory
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public Guid CategoryId { get; set; }
+    }
+}
