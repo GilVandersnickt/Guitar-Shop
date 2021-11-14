2103bebe7e36472a1fad50cb3ddeafca8c7deee7
gilvandersnickt <gil.vandersnickt@student.howest.be>
2021-11-14 22:19:57 +0100

    Add base users

---
+++ src/Imi.Project.Api.Core/Entities/Base/BaseUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Imi.Project.Api.Core.Entities.Base
+{
+    public class BaseUser : IdentityUser
+    {
+        //public Guid UserId { get; set; }
+    }
+}
