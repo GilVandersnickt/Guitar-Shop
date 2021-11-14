12f2ab60ac8dcffdc28f72683c5fa4084e6ec381
gilvandersnickt <gil.vandersnickt@student.howest.be>
2021-11-14 22:24:46 +0100

    Add register dto

---
+++ src/Imi.Project.Api.Core/Dtos/User/RegisterUserRequestDto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Imi.Project.Api.Core.Dtos.User
+{
+    public class RegisterUserRequestDto : DtoBase
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string PasswordConfirm { get; set; }
+
+        //[JsonPropertyName("email_address")]
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public int PostalCode { get; set; }
+        [Required]
+        public string City { get; set; }
+
+    }
+}
