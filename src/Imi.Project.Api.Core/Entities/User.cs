280a4440ede9974cface3e82b7f5b3b73996ccf7
gilvandersnickt <gil.vandersnickt@student.howest.be>
2021-11-14 22:20:10 +0100

    Add user entity

---
+++ src/Imi.Project.Api.Core/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using Imi.Project.Api.Core.Entities.Base;
+using Imi.Project.Api.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imi.Project.Api.Core.Entities
+{
+    public class User : BaseUser
+    {
+        public string Address { get; set; }
+        public int PostalCode { get; set; }
+        public string City { get; set; }
+    }
+}
