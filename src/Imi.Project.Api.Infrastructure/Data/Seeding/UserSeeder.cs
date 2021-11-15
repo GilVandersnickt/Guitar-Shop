3498368d9805b3ed128171e71afd1116b0bd62a1
gilvandersnickt <gil.vandersnickt@student.howest.be>
2021-11-15 03:15:42 +0100

    Update user seeder to add roles

--- src/Imi.Project.Api.Infrastructure/Data/Seeding/UserSeeder.cs
+++ src/Imi.Project.Api.Infrastructure/Data/Seeding/UserSeeder.cs
@@ -10,15 +10,43 @@ namespace Imi.Project.Api.Infrastructure.Data.Seeding
     class UserSeeder
     {
         public static void Seed(ModelBuilder modelBuilder)
-        {            
-            IPasswordHasher<User> passwordHasher = new PasswordHasher<User>();
+        {
+            // Roles
+            Guid SuperAdminRoleId = Guid.Parse("00000001-0000-0000-0000-000000000001");
+            string SuperAdminRoleName = "SuperAdmin";
 
-            const string AdminRoleId = "10000000-0000-0000-0000-000000000001";
-            const string AdminRoleName = "Admin";
+            Guid AdminRoleId = Guid.Parse("00000001-0000-0000-0000-000000000002");
+            string AdminRoleName = "Admin";
 
-            const string AdminUserId = "10000000-0000-0000-0000-000000000001";
+            // SuperAdmin User
+            Guid SuperAdminUserId = Guid.Parse("10000000-0000-0000-0000-000000000001");
+            const string SuperAdminUserName = "superAdmin@guitarshop.com";
+            const string SuperAdminUserPassword = "Test123?";
+
+            // Admin User
+            Guid AdminUserId = Guid.Parse("10000000-0000-0000-0000-000000000002");
             const string AdminUserName = "admin@guitarshop.com";
             const string AdminUserPassword = "Test123?";
+
+            IPasswordHasher<User> passwordHasher = new PasswordHasher<User>();
+
+            var usersToSeed = new List<User>();
+
+            User superAdminUser = new User
+            {
+                Id = SuperAdminUserId,
+                UserName = SuperAdminUserName,
+                NormalizedUserName = SuperAdminUserName.ToUpper(),
+                Email = SuperAdminUserName,
+                NormalizedEmail = SuperAdminUserName.ToUpper(),
+                EmailConfirmed = true,
+                SecurityStamp = "VVPCRDAS3MJWQD5CSW2GWPRADBXEZINB", //Random string
+                ConcurrencyStamp = "c8554266-b401-4519-9aeb-a9283053fc59", //Random guid string
+                Address = "Rijselstraat 5",
+                PostalCode = 8000,
+                City = "Brugge",
+                PhoneNumber = "0490876543",
+            };
 
             User adminUser = new User
             {
@@ -33,58 +61,43 @@ namespace Imi.Project.Api.Infrastructure.Data.Seeding
                 Address = "Rijselstraat 5",
                 PostalCode = 8000,
                 City = "Brugge",
-                PhoneNumber = "0499876543", 
+                PhoneNumber = "0499876543",
             };
 
-            const string TestUserRoleId = "10000000-0000-0000-0000-000000000002";
-            const string TestUserRoleName = "TestUser";
-
-            const string TestUserUserId = "10000000-0000-0000-0000-000000000002";
-            const string TestUserUserName = "testUser@guitarshop.com";
-            const string TestUserUserPassword = "Test123?";
-
-            User testUser = new User
-            {
-                Id = TestUserUserId,
-                UserName = TestUserUserName,
-                NormalizedUserName = TestUserUserName.ToUpper(),
-                Email = TestUserUserName,
-                NormalizedEmail = TestUserUserName.ToUpper(),
-                EmailConfirmed = true,
-                SecurityStamp = "VVPCRDAS3MJWQD5CSW2GWPRADBXEZINB", //Random string
-                ConcurrencyStamp = "c8554266-b401-4519-9aeb-a9283053fc59", //Random guid string
-                Address = "Rijselstraat 5",
-                PostalCode = 8000,
-                City = "Brugge",
-                PhoneNumber = "0490876543", 
-            };
-
-
-
+            superAdminUser.PasswordHash = passwordHasher.HashPassword(superAdminUser, SuperAdminUserPassword);
             adminUser.PasswordHash = passwordHasher.HashPassword(adminUser, AdminUserPassword);
-            testUser.PasswordHash = passwordHasher.HashPassword(testUser, TestUserUserPassword);
 
-            modelBuilder.Entity<IdentityRole>().HasData(new IdentityRole
-            {
-                Id = AdminRoleId,
-                Name = AdminRoleName,
-                NormalizedName = AdminRoleName.ToUpper()
-            });
+            usersToSeed.Add(superAdminUser);
+            usersToSeed.Add(adminUser);
 
-            modelBuilder.Entity<IdentityRole>().HasData(new IdentityRole
-            {
-                Id = TestUserRoleId,
-                Name = TestUserRoleName,
-                NormalizedName = TestUserRoleName.ToUpper()
-            });
+            modelBuilder.Entity<User>().HasData(usersToSeed);
 
-            modelBuilder.Entity<User>().HasData(adminUser);
+            modelBuilder.Entity<IdentityRole<Guid>>().HasData(
+                new IdentityRole<Guid>
+                {
+                    Id = AdminRoleId,
+                    Name = AdminRoleName,
+                    NormalizedName = AdminRoleName.ToUpper()
+                },
+                new IdentityRole<Guid>
+                {
+                    Id = SuperAdminRoleId,
+                    Name = SuperAdminRoleName,
+                    NormalizedName = SuperAdminRoleName.ToUpper()
+                });
 
-            modelBuilder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>
-            {
-                RoleId = AdminRoleId,
-                UserId = AdminUserId
-            });
+            modelBuilder.Entity<IdentityUserRole<Guid>>().HasData(
+                new IdentityUserRole<Guid>
+                {
+                    RoleId = AdminRoleId,
+                    UserId = AdminUserId
+                },
+                new IdentityUserRole<Guid>
+                {
+                    RoleId = SuperAdminRoleId,
+                    UserId = SuperAdminUserId
+                });
+
         }
     }
 }
