9cf27bb90aced10f5130357334c4edef2085ff04
gilvandersnickt <gil.vandersnickt@student.howest.be>
2021-11-21 17:46:00 +0100

    Add admin credentials class

---
+++ src/Imi.Project.Wpf/Constants/AdminCredentials.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imi.Project.Wpf.Constants
+{
+    public class AdminCredentials
+    {
+        public const string Username = "admin@guitarshop.com";
+        public const string Password = "Test123?";
+    }
+}
