b9287c6b39738228a60695584fafe1356168bed6
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-01-07 08:59:28 +0100

    Add view models for brand crud pages

---
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/ViewModels/CRUDBrandUpdateViewModel.cs
@@ -0,0 +1,144 @@
+﻿using FreshMvvm;
+using Imi.Project.Mobile.Domain.Models;
+using Imi.Project.Mobile.Domain.Services;
+using Imi.Project.Mobile.Domain.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Imi.Project.Mobile.ViewModels
+{
+    public class CRUDBrandUpdateViewModel : FreshBasePageModel
+    {
+        private readonly IBrandService brandService;
+
+        public CRUDBrandUpdateViewModel()
+        {
+            brandService = new BrandService();
+        }
+        #region Properties
+        private Brand brandToEdit;
+        public Brand BrandToEdit
+        {
+            get { return brandToEdit; }
+            set
+            {
+                brandToEdit = value;
+                RaisePropertyChanged(nameof(BrandToEdit));
+            }
+        }
+        private ObservableCollection<Brand> brands;
+        public ObservableCollection<Brand> Brands
+        {
+            get { return brands; }
+            set
+            {
+                brands = value;
+                RaisePropertyChanged(nameof(Brands));
+            }
+        }
+        private string brandName;
+        public string BrandName
+        {
+            get { return brandName; }
+            set
+            {
+                brandName = value;
+                RaisePropertyChanged(nameof(BrandName));
+            }
+        }
+        private ImageSource brandImageSource;
+        public ImageSource BrandImageSource
+        {
+            get { return brandImageSource; }
+            set
+            {
+                brandImageSource = value;
+                RaisePropertyChanged(nameof(BrandImageSource));
+            }
+        }
+        #endregion
+
+        #region Commands
+        public ICommand LoadBrand => new Command(
+            () =>
+            {
+                if(BrandToEdit != null)
+                {
+                    BrandImageSource = BrandToEdit.Image;
+                    BrandName = BrandToEdit.Name;
+                }
+            }
+        );
+        public ICommand SaveBrand => new Command(
+            async () =>
+            {
+                if (BrandToEdit != null)
+                {
+                    Brand newBrand = BrandToEdit;
+                    newBrand.Name = BrandName;
+
+                    if (BrandImageSource != null)
+                        newBrand.Image = BrandImageSource.ToString();
+                    else
+                        newBrand.Image = "";
+
+                    var confirmed = await CoreMethods.DisplayAlert("Confirm Edit", "Are you sure you want to edit this brand?", "Yes", "No");
+                    if(confirmed)
+                    {
+                        await brandService.Update(newBrand);
+                        await CoreMethods.PopModalNavigationService();
+                    }
+                }
+            }
+        );
+        public ICommand TakePhoto => new Command(
+            async () =>
+            {
+                var result = await MediaPicker.CapturePhotoAsync();
+                if (result != null)
+                {
+                    var stream = await result.OpenReadAsync();
+                    BrandImageSource = ImageSource.FromStream(() => stream);
+                }
+            }
+        );
+        public ICommand UploadPhoto => new Command(
+            async () =>
+            {
+                var result = await MediaPicker.PickPhotoAsync(new MediaPickerOptions
+                {
+                    Title = "Please choose an image"
+                });
+                if (result != null)
+                {
+                    var stream = await result.OpenReadAsync();
+                    BrandImageSource = ImageSource.FromStream(() => stream);
+                }
+            }
+        );
+        #endregion
+        public async override void Init(object initData)
+        {
+            base.Init(initData);
+
+            await RefreshBrandList();
+        }
+        private async Task RefreshBrandList()
+        {
+            var brands = await brandService.Get();
+            Brands = new ObservableCollection<Brand>(brands);
+
+            if (BrandToEdit == null)
+            {
+                BrandToEdit = new Brand();
+            }
+        }
+    }
+}
---
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/ViewModels/CRUDBrandViewModel.cs
@@ -0,0 +1,14 @@
+﻿using FreshMvvm;
+
+namespace Imi.Project.Mobile.ViewModels
+{
+    public class CRUDBrandViewModel : FreshTabbedNavigationContainer
+    {
+        public CRUDBrandViewModel()
+        {
+            AddTab<CRUDBrandAddViewModel>("Add", null);
+            AddTab<CRUDBrandUpdateViewModel>("Edit", null);
+            AddTab<CRUDBrandDeleteViewModel>("Delete", null);
+        }
+    }
+}
