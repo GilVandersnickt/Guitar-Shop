f5707aa1df2216a30fa93e989af0246e6aedb8fa
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-01-07 08:50:47 +0100

    Add view model for home page

---
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/ViewModels/HomeViewModel.cs
@@ -0,0 +1,34 @@
+﻿using FreshMvvm;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Imi.Project.Mobile.ViewModels
+{
+    public class HomeViewModel : FreshBasePageModel
+    {
+        public HomeViewModel()
+        {
+
+        }
+
+        public ICommand OpenLoginPageCommand => new Command(
+           async () =>
+           {
+               await CoreMethods.PushPageModel<LoginViewModel>(true);
+           }
+        );
+        public ICommand OpenRegisterPageCommand => new Command(
+           async () =>
+           {
+               await CoreMethods.PushPageModel<RegisterViewModel>(true);
+           }
+        );
+        public ICommand OpenAdminPageCommand => new Command(
+           async () =>
+           {
+               await CoreMethods.PushPageModel<AdminViewModel>(true);
+           }
+        );
+
+    }
+}
