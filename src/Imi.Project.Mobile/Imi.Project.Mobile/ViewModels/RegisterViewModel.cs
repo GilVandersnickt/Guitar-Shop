fe3092fabe80d9bc23abdf3e70164b3edc719efe
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-01-07 08:52:43 +0100

    Add view model for register page

---
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,23 @@
+﻿using FreshMvvm;
+using Imi.Project.Mobile.Domain.Models;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Imi.Project.Mobile.ViewModels
+{
+    public class RegisterViewModel : FreshBasePageModel
+    {
+        public RegisterViewModel()
+        {
+
+        }
+        public ICommand OpenMainPageCommand => new Command<User>(
+            async (User user) =>
+            {
+                await CoreMethods.PushPageModel<MainViewModel>(user, false, true);
+            }
+        );
+
+
+    }
+}
