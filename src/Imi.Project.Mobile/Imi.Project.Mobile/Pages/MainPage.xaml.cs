4a162a70e1f4af612a7f25e798b0a54427e61f24
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-01-07 08:52:57 +0100

    Update main page to mvvm

--- src/Imi.Project.Mobile/Imi.Project.Mobile/Pages/MainPage.xaml.cs
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/Pages/MainPage.xaml.cs
@@ -15,54 +15,10 @@ namespace Imi.Project.Mobile.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MainPage : ContentPage
     {
-        private readonly ICategoryService categoryService = new CategoryService();
-        private readonly IBrandService brandService = new BrandService();
-        public List<Category> Categories { get => GetCategories(); }
-        public List<Brand> Brands { get => GetBrands(); }
 
         public MainPage()
         {
             InitializeComponent();
-            this.BindingContext = this;
-        }
-
-
-        private List<Category> GetCategories()
-        {
-            return categoryService.Get().Result;           
-        }
-        private List<Brand> GetBrands()
-        {
-            return brandService.Get().Result;
-        }
-
-        private void Categories_Clicked(object sender, EventArgs e)
-        {
-            var categoriesPage = new CategoriesPage();
-            Navigation.PushAsync(categoriesPage, true);
-        }
-        private void Brands_Clicked(object sender, EventArgs e)
-        {
-            var brandsPage = new BrandsPage();
-            Navigation.PushAsync(brandsPage, true);
-        }
-
-        private async void cvCategories_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            var category = e.CurrentSelection.FirstOrDefault() as Category;
-            if (category == null)
-                return;
-            await Navigation.PushAsync(new ProductsPage(category));
-            ((CollectionView)sender).SelectedItem = null;
-        }
-
-        private async void cvBrands_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            var brand = e.CurrentSelection.FirstOrDefault() as Brand;
-            if (brand == null)
-                return;
-            await Navigation.PushAsync(new ProductsPage(brand));
-            ((CollectionView)sender).SelectedItem = null;
         }
     }
 }
