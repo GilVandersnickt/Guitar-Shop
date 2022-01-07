db5ee03b692f7938afe6bc8a4fb26d2998d333cb
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-01-07 10:52:26 +0100

    Clean interfaces

--- src/Imi.Project.Mobile/Imi.Project.Mobile/Domain/Services/Interfaces/IBrandService.cs
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/Domain/Services/Interfaces/IBrandService.cs
@@ -1,8 +1,4 @@
 ﻿using Imi.Project.Mobile.Domain.Models;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Imi.Project.Mobile.Domain.Services.Interfaces
 {
--- src/Imi.Project.Mobile/Imi.Project.Mobile/Domain/Services/Interfaces/IProductService.cs
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/Domain/Services/Interfaces/IProductService.cs
@@ -1,12 +1,10 @@
 ﻿using Imi.Project.Mobile.Domain.Models;
-using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Imi.Project.Mobile.Domain.Services.Interfaces
 {
-    interface IProductService : ICrudService<Product>
+    public interface IProductService : ICrudService<Product>
     {
         Task<List<Product>> GetProductsByBrand(Brand brand);
         Task<List<Product>> GetProductsByCategory(Category category);
