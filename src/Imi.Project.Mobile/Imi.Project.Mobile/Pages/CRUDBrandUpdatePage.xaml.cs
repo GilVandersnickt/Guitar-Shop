09c3f64f1c17ebd4380ff3a6e9d2294e255a79de
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-01-07 08:59:01 +0100

    Update brand crud pages to mvvm

--- src/Imi.Project.Mobile/Imi.Project.Mobile/Pages/CRUDBrandUpdatePage.xaml.cs
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/Pages/CRUDBrandUpdatePage.xaml.cs
@@ -1,13 +1,4 @@
-﻿using Imi.Project.Mobile.Domain.Models;
-using Imi.Project.Mobile.Domain.Services;
-using Imi.Project.Mobile.Domain.Services.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xamarin.Essentials;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Imi.Project.Mobile.Pages
@@ -15,67 +6,9 @@ namespace Imi.Project.Mobile.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CRUDBrandUpdatePage : ContentPage
     {
-        private readonly IBrandService brandService = new BrandService();
-        public List<Brand> Brands { get => GetBrands(); }
-
-
         public CRUDBrandUpdatePage()
         {
             InitializeComponent();
-            this.BindingContext = this;
-        }
-        private List<Brand> GetBrands()
-        {
-            return brandService.Get().Result;
-        }
-        private async void btnTakePhoto_Clicked(object sender, EventArgs e)
-        {
-            var selectedBrand = ((Button)sender).CommandParameter as Brand;
-            var result = await MediaPicker.CapturePhotoAsync();
-            if (result != null)
-            {
-                var stream = await result.OpenReadAsync();
-                imgPhoto.Source = ImageSource.FromStream(() => stream);
-            }
-
-        }
-        private async void btnUploadPhoto_Clicked(object sender, EventArgs e)
-        {
-            var result = await MediaPicker.PickPhotoAsync(new MediaPickerOptions
-            {
-                Title = "Please choose an image"
-            });
-            if (result != null)
-            {
-                var stream = await result.OpenReadAsync();
-                imgPhoto.Source = ImageSource.FromStream(() => stream);
-            }
-        }
-
-        private async void btnSave_Clicked(object sender, EventArgs e)
-        {
-            if(pkrBrand.SelectedItem != null)
-            {
-                Brand selectedBrand = (Brand)pkrBrand.SelectedItem;
-                Brand brand = brandService.Get(selectedBrand.Id).Result;
-
-                brand.Id = pkrBrand.Id;
-                brand.Name = txtBrandName.Text;
-                if (imgPhoto.Source != null)
-                    brand.Image = imgPhoto.Source.ToString();
-                else
-                    brand.Image = "";
-
-                await brandService.Update(brand);
-                await Navigation.PopAsync();
-            }
-        }
-
-        private void pkrBrand_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            Brand selectedBrand = (Brand)pkrBrand.SelectedItem;
-            imgPhoto.Source = selectedBrand.Image;
-            txtBrandName.Text = selectedBrand.Name;
         }
     }
 }
