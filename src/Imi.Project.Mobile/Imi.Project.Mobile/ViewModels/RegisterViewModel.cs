e5b67cdc5f9fc7c3468c4d8105c1e0cdc005f193
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-01-08 14:27:40 +0100

    Fix navigation in view models

--- src/Imi.Project.Mobile/Imi.Project.Mobile/ViewModels/RegisterViewModel.cs
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/ViewModels/RegisterViewModel.cs
@@ -11,13 +11,36 @@ namespace Imi.Project.Mobile.ViewModels
         {
 
         }
-        public ICommand OpenMainPageCommand => new Command<User>(
+        #region Properties
+        private string userName;
+        public string UserName
+        {
+            get { return userName; }
+            set
+            {
+                userName = value;
+                RaisePropertyChanged(nameof(UserName));
+            }
+        }
+        private string password;
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                password = value;
+                RaisePropertyChanged(nameof(Password));
+            }
+        }
+        #endregion
+        #region Commands
+        public ICommand Register => new Command<User>(
             async (User user) =>
             {
                 await CoreMethods.PushPageModel<MainViewModel>(user, false, true);
             }
         );
-
+        #endregion
 
     }
 }
