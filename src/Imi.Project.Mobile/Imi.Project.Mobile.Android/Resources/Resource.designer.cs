6ccd8d42fe4d89e9e4cb8dcfb3b0c18941af8d1d
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-01-08 14:25:17 +0100

    Add placeholder image

--- src/Imi.Project.Mobile/Imi.Project.Mobile.Android/Resources/Resource.designer.cs
+++ src/Imi.Project.Mobile/Imi.Project.Mobile.Android/Resources/Resource.designer.cs
@@ -16930,37 +16930,40 @@ namespace Imi.Project.Mobile.Droid
 			public const int notify_panel_notification_icon_bg = 2131165342;
 			
 			// aapt resource value: 0x7F07009F
-			public const int preference_list_divider_material = 2131165343;
+			public const int Placeholder = 2131165343;
 			
 			// aapt resource value: 0x7F0700A0
-			public const int Roland = 2131165344;
+			public const int preference_list_divider_material = 2131165344;
 			
 			// aapt resource value: 0x7F0700A1
-			public const int rolandricbpcpatchkabel015mACC0007020000 = 2131165345;
+			public const int Roland = 2131165345;
 			
 			// aapt resource value: 0x7F0700A2
-			public const int rolandricg10agoldseriesACC0007014000 = 2131165346;
+			public const int rolandricbpcpatchkabel015mACC0007020000 = 2131165346;
 			
 			// aapt resource value: 0x7F0700A3
-			public const int test_custom_background = 2131165347;
+			public const int rolandricg10agoldseriesACC0007014000 = 2131165347;
 			
 			// aapt resource value: 0x7F0700A4
-			public const int tooltip_frame_dark = 2131165348;
+			public const int test_custom_background = 2131165348;
 			
 			// aapt resource value: 0x7F0700A5
-			public const int tooltip_frame_light = 2131165349;
+			public const int tooltip_frame_dark = 2131165349;
 			
 			// aapt resource value: 0x7F0700A6
-			public const int Yamaha = 2131165350;
+			public const int tooltip_frame_light = 2131165350;
 			
 			// aapt resource value: 0x7F0700A7
-			public const int yamahac40mmatGIT0019253000 = 2131165351;
+			public const int Yamaha = 2131165351;
 			
 			// aapt resource value: 0x7F0700A8
-			public const int yamahacs40nt34naturalGIT0000644000 = 2131165352;
+			public const int yamahac40mmatGIT0019253000 = 2131165352;
 			
 			// aapt resource value: 0x7F0700A9
-			public const int yamahathr30iiwirelessGIT0051343000 = 2131165353;
+			public const int yamahacs40nt34naturalGIT0000644000 = 2131165353;
+			
+			// aapt resource value: 0x7F0700AA
+			public const int yamahathr30iiwirelessGIT0051343000 = 2131165354;
 			
 			static Drawable()
 			{
