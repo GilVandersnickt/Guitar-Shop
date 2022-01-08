c5d59678c28643bcf526a6c377917ca6a34842e3
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-01-08 14:20:47 +0100

    Update interface product service

--- src/Imi.Project.Mobile/Imi.Project.Mobile/Domain/Services/Interfaces/IProductService.cs
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/Domain/Services/Interfaces/IProductService.cs
@@ -1,11 +1,18 @@
 ﻿using Imi.Project.Mobile.Domain.Models;
+using Imi.Project.Mobile.Domain.Models.Api;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Imi.Project.Mobile.Domain.Services.Interfaces
 {
-    public interface IProductService : ICrudService<Product>
+    public interface IProductService
     {
+        Task<Product> Get(Guid id);
+        Task<List<Product>> Get();
+        Task<ProductRequest> Update(ProductRequest entity);
+        Task<Product> Delete(Guid id);
+        Task<ProductRequest> Add(ProductRequest entity);
         Task<List<Product>> GetProductsByBrand(Brand brand);
         Task<List<Product>> GetProductsByCategory(Category category);
     }
