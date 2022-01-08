1f6556d4d86a4ad0c99ee587ac371de407da4c06
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-01-08 14:30:17 +0100

    Update app class to use api services

--- src/Imi.Project.Mobile/Imi.Project.Mobile/App.xaml.cs
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using FreshMvvm;
 using Imi.Project.Mobile.Domain.Services;
+using Imi.Project.Mobile.Domain.Services.Api;
 using Imi.Project.Mobile.Domain.Services.Interfaces;
 using Imi.Project.Mobile.ViewModels;
 using Xamarin.Forms;
@@ -18,9 +19,11 @@ namespace Imi.Project.Mobile
         {
             //register dependencies
             FreshIOC.Container.Register<IProductService>(new ProductService());
-            FreshIOC.Container.Register<IBrandService>(new BrandService());
-            FreshIOC.Container.Register<ICategoryService>(new CategoryService());
+            FreshIOC.Container.Register<IBrandService>(new ApiBrandsService());
+            FreshIOC.Container.Register<ICategoryService>(new ApiCategoryService());
+            FreshIOC.Container.Register<IUserService>(new ApiUserService());
         }
+
         protected override void OnStart()
         {
         }
