343fccfdf60204f095e32ecc679b0a9c9bce2205
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-01-08 14:20:17 +0100

    Add web api client class

--- src/Imi.Project.Mobile/Imi.Project.Mobile/Domain/Services/Api/WebApiClient.cs
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/Domain/Services/Api/WebApiClient.cs
@@ -1,7 +1,14 @@
-﻿using Newtonsoft.Json;
+﻿using Imi.Project.Mobile.Constants;
+using Imi.Project.Mobile.Domain.Models.Api;
+using Imi.Project.Mobile.Domain.Models.Login;
+using Newtonsoft.Json;
+using System;
 using System.Net.Http;
 using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace Imi.Project.Mobile.Domain.Services.Api
 {
@@ -17,16 +24,15 @@ namespace Imi.Project.Mobile.Domain.Services.Api
 #endif
             return httpClientHandler;
         }
-
         private static JsonMediaTypeFormatter GetJsonFormatter()
         {
             var formatter = new JsonMediaTypeFormatter();
             //prevent self-referencing loops when saving Json (Bucket -> BucketItem -> Bucket -> ...)
             formatter.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+
             return formatter;
         }
-
-        public async static Task<T> GetApiResult<T>(string uri)
+        public static async Task<T> GetApiResult<T>(string uri)
         {
             using (HttpClient httpClient = new HttpClient(ClientHandler()))
             {
@@ -34,32 +40,34 @@ namespace Imi.Project.Mobile.Domain.Services.Api
                 return JsonConvert.DeserializeObject<T>(response, GetJsonFormatter().SerializerSettings);
             }
         }
-
-        public static async Task<TOut> PutCallApi<TOut, TIn>(string uri, TIn entity)
-        {
-            return await CallApi<TOut, TIn>(uri, entity, HttpMethod.Put);
-        }
-
         public static async Task<TOut> PostCallApi<TOut, TIn>(string uri, TIn entity)
         {
             return await CallApi<TOut, TIn>(uri, entity, HttpMethod.Post);
         }
-
+        public static async Task<TOut> PutCallApi<TOut, TIn>(string uri, TIn entity)
+        {
+            return await CallApi<TOut, TIn>(uri, entity, HttpMethod.Put);
+        }
         public static async Task<TOut> DeleteCallApi<TOut>(string uri)
         {
             return await CallApi<TOut, object>(uri, null, HttpMethod.Delete);
         }
-
         private static async Task<TOut> CallApi<TOut, TIn>(string uri, TIn entity, HttpMethod httpMethod)
         {
             TOut result = default;
 
             using (HttpClient httpClient = new HttpClient(ClientHandler()))
             {
+                if (Application.Current.Properties["Token"] != null)
+                    httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Application.Current.Properties["Token"] as string);
+
                 HttpResponseMessage response;
                 if (httpMethod == HttpMethod.Post)
                 {
-                    response = await httpClient.PostAsync(uri, entity, GetJsonFormatter());
+                    var content = JsonConvert.SerializeObject(entity);
+                    var stringContent = new StringContent(content);
+                    stringContent.Headers.ContentType = new MediaTypeHeaderValue("text/plain");
+                    response = await httpClient.PostAsync(uri, stringContent);
                 }
                 else if (httpMethod == HttpMethod.Put)
                 {
@@ -73,6 +81,22 @@ namespace Imi.Project.Mobile.Domain.Services.Api
             }
             return result;
         }
+        public static async Task<bool> LoginCallApi(LoginRequest request)
+        {
+            using (HttpClient httpClient = new HttpClient(ClientHandler()))
+            {
+                httpClient.BaseAddress = new Uri(ApiSettings.BaseUri);
 
+                var response = await httpClient.PostAsync("auth/login", request, GetJsonFormatter());
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseString = await response.Content.ReadAsStringAsync();
+                    var loginResponse = JsonConvert.DeserializeObject<LoginResponse>(responseString);
+                    Application.Current.Properties["Token"] = loginResponse.jwtToken;
+                }
+                return response.IsSuccessStatusCode;
+            }
+        }
     }
 }
