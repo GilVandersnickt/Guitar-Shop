4cf63af989903b583389923557a95e13d7132ad9
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-01-08 14:16:34 +0100

    Add login request class

---
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/Domain/Models/Login/LoginRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Imi.Project.Mobile.Domain.Models.Login
+{
+    public class LoginRequest
+    {
+        //[JsonPropertyName("userName")]
+        public string UserName { get; set; }
+
+        //[JsonPropertyName("password")]
+        public string Password { get; set; }
+    }
+}
