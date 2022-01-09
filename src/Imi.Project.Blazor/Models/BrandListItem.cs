18710092bd2dd4267d2a4456cc93d256bfaa438f
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-01-09 08:06:11 +0100

    Add brand models

---
+++ src/Imi.Project.Blazor/Models/BrandListItem.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Imi.Project.Blazor.Models
+{
+    public class BrandListItem
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
