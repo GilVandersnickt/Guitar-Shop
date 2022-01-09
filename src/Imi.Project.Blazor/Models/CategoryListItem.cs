c31abcc0e302900e3adf23222590053ad600a5db
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-01-09 22:09:23 +0100

    Update id of list item models to string

--- src/Imi.Project.Blazor/Models/CategoryListItem.cs
+++ src/Imi.Project.Blazor/Models/CategoryListItem.cs
@@ -4,7 +4,7 @@ namespace Imi.Project.Blazor.Models
 {
     public class CategoryListItem
     {
-        public Guid Id { get; set; }
+        public string Id { get; set; }
         public string Name { get; set; }
         public string Image { get; set; }
     }
