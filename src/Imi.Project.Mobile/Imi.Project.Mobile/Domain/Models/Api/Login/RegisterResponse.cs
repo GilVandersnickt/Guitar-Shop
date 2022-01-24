f67601dbf4f08668e4dc95fd06cbec33ef2c5b1e
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-01-24 05:11:15 +0100

    Add register models for api

---
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/Domain/Models/Api/Login/RegisterResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imi.Project.Mobile.Domain.Models.Api.Login
+{
+    public class RegisterResponse
+    {
+        public bool Succeeded { get; set; }
+        public ICollection<string> ErrorMessages { get; set; }
+    }
+}
