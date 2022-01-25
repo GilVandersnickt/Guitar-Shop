b5db6af23459760da1ace9b507ff56796ce353a6
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-01-25 20:07:24 +0100

    Add data attribute for minimum age

---
+++ src/Imi.Project.Api.Core/Services/Validation/MinimumAgeAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Imi.Project.Api.Core.Services.Validation
+{
+    public class MinimumAgeAttribute : ValidationAttribute
+    {
+        int _minAge;
+        public MinimumAgeAttribute(int minAge)
+        {
+            _minAge = minAge;
+        }
+        public override bool IsValid(object value)
+        {
+            DateTime date;
+            if (DateTime.TryParse(value.ToString(), out date))
+            {
+                return date.AddYears(_minAge) < DateTime.Now;
+            }
+            return false;
+        }
+    }
+}
