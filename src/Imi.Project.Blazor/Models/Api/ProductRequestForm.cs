e4a43a5616d1b8e131d74192b513a87be069aa69
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-01-27 05:48:24 +0100

    Add product models for api

---
+++ src/Imi.Project.Blazor/Models/Api/ProductRequestForm.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Imi.Project.Blazor.Models.Api
+{
+    public class ProductRequestForm : ProductRequest
+    {
+        public decimal PriceDecimal { get; set; }
+        public List<BrandResponse> Brands { get; set; }
+        public List<CategoryResponse> Categories { get; set; }
+        public List<SubcategoryResponse> Subcategories { get; set; }
+        public Stream ImageInputFile { get; set; }
+    }
+}
