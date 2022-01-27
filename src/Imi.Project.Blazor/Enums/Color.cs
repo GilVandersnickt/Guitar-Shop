5ab8cbe1e697a75cfd332b912115b781662ec794
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-01-27 05:45:35 +0100

    Update enum color

--- src/Imi.Project.Blazor/Enums/Color.cs
+++ src/Imi.Project.Blazor/Enums/Color.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Imi.Project.Blazor.Enums
+﻿namespace Imi.Project.Blazor.Enums
 {
     public enum Color
     {
-        red, green, blue, yellow, cyan, orange, pink, lightblue, grey
+        red, green, blue, salmon, cyan, olive, hotpink, indigo, grey, aquamarine
     }
 }
