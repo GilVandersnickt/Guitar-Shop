7d665ad40819ce170567d5971f364bf1f24fc1b4
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-01-27 05:51:26 +0100

    Update interface for brand service

---
+++ src/Imi.Project.Blazor/Services/Interfaces/IBrandService.cs
@@ -0,0 +1,11 @@
+﻿using Imi.Project.Blazor.Models.Api;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Blazor.Services.Interfaces
+{
+    public interface IBrandService
+    {
+        Task<List<BrandResponse>> Get();
+    }
+}
