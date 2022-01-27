58b613c3747172f08f7e268a3a68864dbe860dbc
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-01-27 05:49:07 +0100

    Add subcategory models for api

---
+++ src/Imi.Project.Blazor/Models/Api/SubcategoryRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Imi.Project.Blazor.Models.Api
+{
+    public class SubcategoryRequest
+    {
+        public Guid Id { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "Name is too long.")]
+        public string Name { get; set; }
+        public Guid CategoryId { get; set; }
+    }
+}
---
+++ src/Imi.Project.Blazor/Models/Api/SubcategoryResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Imi.Project.Blazor.Models.Api
+{
+    public class SubcategoryResponse
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+    }
+}
