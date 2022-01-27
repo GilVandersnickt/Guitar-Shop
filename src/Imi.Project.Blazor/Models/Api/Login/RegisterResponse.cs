39df6b383f19c764dfbe8b48a30c18682262864a
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-01-27 05:49:40 +0100

    Add register models for api

---
+++ src/Imi.Project.Blazor/Models/Api/Login/RegisterResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Imi.Project.Blazor.Models.Api.Login
+{
+    public class RegisterResponse
+    {
+        public bool Succeeded { get; set; }
+        public ICollection<string> ErrorMessages { get; set; }
+    }
+}
