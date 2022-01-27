6937682a20cda3104ebe274519b97910764edffa
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-01-27 05:52:26 +0100

    Update api brand service

---
+++ src/Imi.Project.Blazor/Services/Api/ApiBrandService.cs
@@ -0,0 +1,18 @@
+﻿using Imi.Project.Blazor.Constants;
+using Imi.Project.Blazor.Models.Api;
+using Imi.Project.Blazor.Services.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Blazor.Services.Api
+{
+    public class ApiBrandService : IBrandService
+    {
+        public async Task<List<BrandResponse>> Get()
+        {
+            var brands = await WebApiClient.GetApiResult<List<BrandResponse>>($"{ApiSettings.BaseUri}brands");
+            return brands.ToList();
+        }
+    }
+}
