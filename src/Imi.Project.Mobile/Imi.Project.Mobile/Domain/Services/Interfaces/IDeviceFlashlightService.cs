cd1fb9950dd74ca9b3d97c0a2f44515159cc2690
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-01-28 05:14:34 +0100

    Add interface device flashlight service

---
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/Domain/Services/Interfaces/IDeviceFlashlightService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imi.Project.Mobile.Domain.Services.Interfaces
+{
+    public interface IDeviceFlashlightService
+    {
+        void TurnOn();
+        void TurnOff();
+    }
+}
